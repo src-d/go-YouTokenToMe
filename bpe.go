9bbc64d140540556aa1f1b393ff4a25202b34d45
Irina Khismatullina <irenekhismatullina@gmail.com>
2019-10-17 16:43:01 +0300

    Remove text model dump support
    Signed-off-by: Irina Khismatullina <irenekhismatullina@gmail.com>

--- bpe.go
+++ bpe.go
@@ -3,7 +3,6 @@ package bpe
 import (
 	"bufio"
 	"encoding/binary"
-	"fmt"
 	"io"
 
 	"github.com/sirupsen/logrus"
@@ -62,58 +61,6 @@ func DecodeToken(token EncodedToken, id2char map[TokenID]rune) (string, error) {
 		}
 	}
 	return word, nil
-}
-
-// ReadModelFromText loads the BPE model from the text dump
-func ReadModelFromText(reader io.Reader) (*Model, error) {
-	scanner := bufio.NewScanner(reader)
-	var nChars, nRules int
-	scanner.Scan()
-	_, err := fmt.Sscanf(scanner.Text(), "%d %d", &nChars, &nRules)
-	if err != nil {
-		logrus.Fatal("Wrong input format: ", err)
-		return &Model{}, err
-	}
-	model := newModel(nRules)
-	for i := 0; i < nChars; i++ {
-		var char rune
-		var charID TokenID
-		scanner.Scan()
-		_, err = fmt.Sscanf(scanner.Text(), "%d %d", &char, &charID)
-		if err != nil {
-			logrus.Fatal("Wrong input format: ", err)
-			return model, err
-		}
-		model.char2id[char] = charID
-		model.id2char[charID] = char
-		model.recipe[charID] = EncodedToken{charID}
-		model.revRecipe[string(char)] = charID
-	}
-	for i := 0; i < nRules; i++ {
-		var rule rule
-		scanner.Scan()
-		_, err = fmt.Sscanf(scanner.Text(), "%d %d %d", &rule.left, &rule.right, &rule.result)
-		if err != nil {
-			logrus.Fatal("Wrong input format: ", err)
-			return model, err
-		}
-		model.rules[i] = rule
-		model.recipe[rule.result] = append(model.recipe[rule.left], model.recipe[rule.right]...)
-		resultString, err := DecodeToken(model.recipe[rule.result], model.id2char)
-		if err != nil {
-			logrus.Fatal("Unexpected token id inside the rules: ", err)
-			return model, err
-		}
-		model.revRecipe[resultString] = rule.result
-	}
-	scanner.Scan()
-	_, err = fmt.Sscanf(scanner.Text(), "%d %d %d %d", &model.specialTokens.unk,
-		&model.specialTokens.pad, &model.specialTokens.bos, &model.specialTokens.eos)
-	if err != nil {
-		logrus.Fatal("Wrong input format: ", err)
-		return model, err
-	}
-	return model, nil
 }
 
 func specialTokensToBin(specials specialTokens) []byte {
--- bpe_test.go
+++ bpe_test.go
@@ -2,7 +2,6 @@ package bpe
 
 import (
 	"bytes"
-	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -18,32 +17,6 @@ func TestDecodedTokenToString(t *testing.T) {
 	word, err := DecodeToken(EncodedToken{1, 2, 1, 3, 3}, id2char)
 	require.NoError(t, err)
 	require.Equal(t, "abacc", word)
-}
-
-func TestReadModelFromText(t *testing.T) {
-	reader := strings.NewReader(`5 4
-99 6
-98 7
-95 4
-100 5
-97 8
-4 8 9
-4 6 10
-4 5 11
-4 7 12
-1 0 2 3`)
-	expected := Model{
-		map[rune]TokenID{97: 8, 98: 7, 99: 6, 100: 5, 95: 4},
-		map[TokenID]rune{4: 95, 5: 100, 6: 99, 7: 98, 8: 97},
-		[]rule{{4, 8, 9}, {4, 6, 10}, {4, 5, 11}, {4, 7, 12}},
-		map[TokenID]EncodedToken{4: {4}, 5: {5}, 6: {6}, 7: {7}, 8: {8}, 9: {4, 8}, 10: {4, 6}, 11: {4, 5}, 12: {4, 7}},
-		map[string]TokenID{"a": 8, "b": 7, "c": 6, "d": 5, "_": 4,
-			"_a": 9, "_b": 12, "_c": 10, "_d": 11},
-		specialTokens{1, 0, 2, 3},
-	}
-	model, err := ReadModelFromText(reader)
-	require.NoError(t, err)
-	require.Equal(t, expected, *model)
 }
 
 func TestSpecialTokensToBin(t *testing.T) {
